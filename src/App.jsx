21bf93a4317d252fd2d173f6577d45375bc97940
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-08-15 10:56:30 +0200

    Added the community component and the Founder component along with the data for the community

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import { Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+
 import Layout from "./Layouts/Layout/Layout";
-import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-import Service from "./Pages/Services/Service";
-import Products from "./Pages/Products/Products";
-import ContactUs from "./Pages/Contact_Us/ContactUs";
+import Loader from "./Components/Loader/Loader";
+const Home = React.lazy(() => import("./Pages/Home/Home"));
+const About = React.lazy(() => import("./Pages/About/About"));
+const Service = React.lazy(() => import("./Pages/Services/Service"));
+const Products = React.lazy(() => import("./Pages/Products/Products"));
+const ContactUs = React.lazy(() => import("./Pages/Contact_Us/ContactUs"));
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="text-center w-screen h-screen grid place-items-center">
+            <Loader className="text-center w-screen h-screen grid place-items-center" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/product" element={<Products />} />
+            <Route path="/contact" element={<ContactUs />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
